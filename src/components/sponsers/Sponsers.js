a8cf07ae237dd99d8aab3c5159d981f68794bd34
deepamarti <deepamarti@gmail.com>
2021-09-25 11:32:50 -0700

    sponsors

--- src/components/sponsers/Sponsers.js
+++ src/components/sponsers/Sponsers.js
@@ -7,7 +7,7 @@ const Sponsers = () => {
       className="sponsersSection d-flex flex-column p-2 m-2 mt-5 py-md-4 px-md-3 m-md-4"
       id="sponsersSectionId"
     >
-      <h1 id="main-heading">Sponsers</h1>
+      <h1 id="main-heading">Sponsors</h1>
       
       <a href="https://www.numerade.com/">
          <img alt="numerade-logo" src={Numerade} width="464.012738854" height="100"/>
