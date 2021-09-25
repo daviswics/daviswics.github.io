ad78970b73413fd93864bf4592aacf98d27e044a
deepamarti <deepamarti@gmail.com>
2021-09-25 11:12:03 -0700

    add new events

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -9,35 +9,49 @@ const eventList = [
   {
     id: 2,
     date: "Oct. 4, 2021, 7-8PM PT",
-    name: "📄Resume Review w/ Ridgeline",
-    type: "virtual",
+    name: "Decoding the Tech Recruitment Process",
+    type: "in-person",
     link: "https://tinyurl.com/WomeninCSMeeting" 
   },
   {
     id: 3,
-    date: "Oct. 11, 2021, 6-7PM PT",
-    name: "🟠API Workshop w/ Postman",
+    date: "Oct. 11, 2021, 7-8PM PT",
+    name: "Interview Prep Workshop with ServiceNow",
     type: "in-person",
     link: "https://tinyurl.com/WomeninCSMeeting"
   },
   {
     id: 4,
-    date: "Oct. 18, 2021 (tentative)",
-    name: "🍿Game Night Socials",
+    date: "Oct. 18, 2021  7-8PM PT",
+    name: "Workshop: Create your own Website using GitHub Pages",
     type: "in-person",
     link: "https://tinyurl.com/WomeninCSMeeting"
   },
   {
     id: 5,
     date: "Oct 25, 2021, 7-8PM PT",
-    name: "💼Day in life of Tech Consultant w/ FAST Enterprise",
+    name: "Tech Career Panel with Centene and New Relic",
     type: "in-person",
     link: "https://tinyurl.com/WomeninCSMeeting"
   },
   {
     id: 6,
     date: "Nov 1, 2021 7-8PM PT",
-    name: "💻Product Management Workshop",
+    name: "LeetCode & Boba",
+    type: "in-person",
+    link: "https://tinyurl.com/WomeninCSMeeting"
+  },
+  {
+    id: 7,
+    date: "Nov 8, 2021 7-8PM PT",
+    name: "Tech Talks and Chipotle with Sandia Labs",
+    type: "in-person",
+    link: "https://tinyurl.com/WomeninCSMeeting"
+  },
+  {
+    id: 8,
+    date: "Nov 15, 2021 7-8PM PT",
+    name: "WiCS Social",
     type: "in-person",
     link: "https://tinyurl.com/WomeninCSMeeting"
   }
