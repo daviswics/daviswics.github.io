49f2f77474c765e2b091e34752a7de13d1ecd209
Christina Huang <christinahuangji@gmail.com>
2022-01-15 19:02:20 -0800

    sample joke and coding question for newsletter

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -7,16 +7,42 @@ const Newsletter = () => {
       id="newsletterSectionId"
     >
       <h1 id="main-heading">Newsletter</h1>
+      <h5 id="sub-heading">
+        Joke of the Week Sneak Peek
+      </h5>
       <p>
-      Q: How do front end devs like their brownies? <br/>
-      A: GUI
+        Q: What kind of microchip does this new vaccine contain, can I install a Linux based OS on it? <br/>
+        A: I don't know for sure, but it must be ARM based.
+      </p>
+      <h5 id="sub-heading">
+        Coding Question of the Week Sneak Peek
+      </h5>
+      <p>
+        You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- every time you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block.
       </p>
       <p>
-        Fun tidbits you're signing up for include CS dad jokes,
+        Create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise. Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+      </p>
+      <h5 id="sub-heading">
+        What's in the newsletter?
+      </h5>
+      <p>
+        We write the WiCS weekly newsletter to lighten your mood with a
+        Joke of the Week, challenge your coding skills with a Coding Challenge,
+        and offer opportunities to apply to Scholarships, Internships, and
+        attend Events from companies and other clubs at Davis.
+        We would love to hear your feedback and learn how we can better
+        support you! You can contact us by messaging us in the WiCS discord,
+        shooting us a quick email, or filling out the newsletter feedback form:
+        {" "}
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdnlof5ej1o3A0yfGyXYI3nGbzawvWp-XicfbHEFqigC2y7qA/viewform"> Feedback Form </a>
+      </p>
+      <p>
+        Sign up for our newsletter to receive meeting reminders, CS dad jokes,
         internship/scholarship opportunities, and tech event/conference invites.
       </p>
       <h3 id="sub-heading">
-        Sign up for our newsletter to get meeting reminders! {" "}
+        {" "}
         <a href="https://tinyletter.com/wicsdavis"> Link to TinyLetter Sign-Up Page</a>
       </h3>
     </div>
