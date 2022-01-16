89eebbe0e5cd1001e19a8eafc0e37f7184b252ff
Christina Huang <christinahuangji@gmail.com>
2022-01-15 19:11:49 -0800

    remove coding question and add emojis

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -8,23 +8,14 @@ const Newsletter = () => {
     >
       <h1 id="main-heading">Newsletter</h1>
       <h5 id="sub-heading">
-        Joke of the Week Sneak Peek
+      🌱Joke of the Week Sneak Peek
       </h5>
       <p>
-        Q: What kind of microchip does this new vaccine contain, can I install a Linux based OS on it? <br/>
-        A: I don't know for sure, but it must be ARM based.
+        Q: What kind of microchip does this new vaccine contain, can I install a Linux based OS on it?💉 <br/>
+        A: I don't know for sure, but it must be ARM based.💪
       </p>
       <h5 id="sub-heading">
-        Coding Question of the Week Sneak Peek
-      </h5>
-      <p>
-        You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- every time you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block.
-      </p>
-      <p>
-        Create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise. Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
-      </p>
-      <h5 id="sub-heading">
-        What's in the newsletter?
+      🍓What's in the newsletter?
       </h5>
       <p>
         We write the WiCS weekly newsletter to lighten your mood with a
