ea00f6d5d26779519956109d12a357bab0d02bfd
Christina Huang <christinahuangji@gmail.com>
2022-01-15 18:49:27 -0800

    elaborate in newsletter section

--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -6,10 +6,18 @@ const Newsletter = () => {
       className="newletterSection d-flex flex-column p-2 m-2 mt-5 py-md-4 px-md-3 m-md-4"
       id="newsletterSectionId"
     >
-      <h1 id="main-heading">Want to learn more?</h1>
+      <h1 id="main-heading">Newsletter</h1>
+      <p>
+      Q: How do front end devs like their brownies? <br/>
+      A: GUI
+      </p>
+      <p>
+        Fun tidbits you're signing up for include CS dad jokes,
+        internship/scholarship opportunities, and tech event/conference invites.
+      </p>
       <h3 id="sub-heading">
-        Sign up for our newsletter:{" "}
-        <a href="https://tinyletter.com/wicsdavis"> Here</a>
+        Sign up for our newsletter to get meeting reminders! {" "}
+        <a href="https://tinyletter.com/wicsdavis"> Link to TinyLetter Sign-Up Page</a>
       </h3>
     </div>
   );
