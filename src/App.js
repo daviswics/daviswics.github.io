8795a81a31ac8edecf242c8d661b29bcdb4fda8b
Shimona Prabhu <0718shimona@gmail.com>
2023-02-23 23:35:56 -0700

    Commenting out events

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import MyNavbar from "./components/common/Navbar";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Events from "./components/events/Events";
-import PastEvents from "./components/pastevents/PastEvents";
+// import Events from "./components/events/Events";
+// import PastEvents from "./components/pastevents/PastEvents";
 import Newsletter from "./components/newsletter/Newsletter";
 import Contact from "./components/contact/Contact";
 
@@ -15,8 +15,8 @@ function App() {
       <Home />
       <About />
       <Newsletter />
-      <Events/>
-      <PastEvents/>
+      {/* <Events/>
+      <PastEvents/> */}
       <Contact />
     </div>
   );
